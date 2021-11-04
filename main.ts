e725786a100f836b638a47be27cf815a056f9c35
Limezy <arnaudetpia@de-ruffray.fr>
2021-11-04 17:26:20 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -9,7 +9,7 @@ export default class LinkHeadersDirectly extends Plugin {
 		postProc = (el: HTMLElement, ctx: MarkdownPostProcessorContext) => {
 
 			let linkElements = el.querySelectorAll('a.internal-link');
-			let barIndex, alias;
+			let barIndex, alias,alias2;
 
 			for(let i = 0; i < linkElements.length; i++) {
 
@@ -17,7 +17,12 @@ export default class LinkHeadersDirectly extends Plugin {
 
 				barIndex = linkAsHTML.indexOf(">");
 				if(barIndex < 0) continue;
-				alias = string.concat(linkAsHTML.substr(0,barIndex),",",linkAsHTML.substr(barIndex+2));
+				alias = linkAsHTML.substr(0,barIndex);
+				alias2 = linkAsHTML.substr(barIndex+2);
+				console.log("Alias:"+alias)
+				console.log("Alias2"+alias2)
+				alias.concat(",",alias2);
+				console.log("Test:"+alias);
 				(linkElements[i] as HTMLElement).innerText = alias;
 			}
 		}
